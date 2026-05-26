8455c7d99a6494a4c1c52c8f312dcf0b77a46206
EmilyRl08 <emily.lourenco@aluno.senai.br>
2026-05-26 14:25:30 -0300

    Update: atualizando homepage e corrigindo o package.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://github.com/EmilyRl08/Livreando.git', // 👈 ADICIONE ESSA LINHA (mude para o nome real do seu repo)
 })
