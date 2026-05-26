af645cd605e20a25f839ee1ae2502a76b1652554
EmilyRl08 <emily.lourenco@aluno.senai.br>
2026-05-26 16:26:31 -0300

    Fix: ajustando o base path do vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/EmilyRl08/Livreando.git', // 👈 ADICIONE ESSA LINHA (mude para o nome real do seu repo)
+  base: '/Livreando/', // 🌟 Corrigido: Apenas o nome do repositório entre barras
 })
